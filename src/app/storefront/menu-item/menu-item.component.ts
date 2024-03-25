7c532c10fb8557d8819bee1b183dc8cf19ccc4a5
stephen <sely1986@gmail.com>
2024-03-24 20:49:21 -0400

    test cicd

--- src/app/storefront/menu-item/menu-item.component.ts
+++ src/app/storefront/menu-item/menu-item.component.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-item',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.css',
 })
 export class MenuItemComponent {
-  constructor() {
-    console.log('hello?');
-  }
+  newMenuItemForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    image: new FormControl(null),
+  });
+  route = inject(Router);
+  submitNewMenuItem(...all: unknown[]) {}
 }
